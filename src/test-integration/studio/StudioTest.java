fd817c1ab6bb559027d3c81c278406fd8b7ceff4
sylvain <8940871+punx120@users.noreply.github.com>
2022-01-27 14:56:26 -0500

    StudioTest - Use different tmp dir

--- src/test-integration/studio/StudioTest.java
+++ src/test-integration/studio/StudioTest.java
@@ -1,6 +1,9 @@
 package studio;
 
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.assertj.swing.core.KeyPressInfo;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
@@ -8,8 +11,8 @@ import org.assertj.swing.fixture.JTextComponentFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Assert;
 import org.junit.Test;
-import studio.ui.StudioPanel;
 import studio.core.Studio;
+import studio.ui.StudioPanel;
 
 public class StudioTest extends AssertJSwingJUnitTestCase {
 
@@ -17,12 +20,17 @@ public class StudioTest extends AssertJSwingJUnitTestCase {
 
     @Override
     protected void onSetUp() {
-        System.setProperty("user.home", "/tmp/");
-        Studio.init0();
-        StudioPanel panel = GuiActionRunner.execute(() -> Studio.createPanel(new String[0]));
-        Assert.assertNotEquals("panel is not null", null, panel);
-        window = new FrameFixture(robot(), panel.frame());
-        window.show(); // shows the frame to test
+        try {
+            Path tempDirectory = Files.createTempDirectory(System.getProperty("user.name"));
+            System.setProperty("user.home", tempDirectory.toString());
+            Studio.init0();
+            StudioPanel panel = GuiActionRunner.execute(() -> Studio.createPanel(new String[0]));
+            Assert.assertNotEquals("panel is not null", null, panel);
+            window = new FrameFixture(robot(), panel.frame());
+            window.show(); // shows the frame to test
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void type(JTextComponentFixture tb, String text) {
